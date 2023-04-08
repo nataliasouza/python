112da2e1c3539fa6ab3f15438d58667166f50a50
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-04-08 20:00:29 -0300

    Precedência das operações

---
+++ python/Livro/precedenciaOperacoes.py
@@ -0,0 +1,25 @@
+#1 - Primeiro resolvem-se os parênteses, do mais interno para o mais externo
+#2 - Exponenciação
+#3 - Multiplicação
+#4 - Soma e subtração
+
+#Qual o resultado armazenado na variável operacao_1: 17 ou 25?
+
+operacao_1 = 2 + 3 * 5
+print (f"Resultado em operacao_1 = {operacao_1}")
+
+#Qual o resultado armazenado na variável operacao_2: 17 ou 25?
+
+operacao_2 = (2 + 3) * 5
+print (f"Resultado em operacao_2 = {operacao_2}")
+
+#Qual o resultado armazenado na variável operacao_3: 4 ou 1?
+
+operacao_3 = 4 / 2 ** 2
+print (f"Resultado em operacao_3 = {operacao_3}")
+
+#Qual o resultado armazenado na variável operacao_4: 4 ou 1?
+
+operacao_4 = 13 % 3 + 4
+print (f"Resultado em operacao_4 = {operacao_4}")
+
