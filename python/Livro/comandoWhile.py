54b775635abe88490c6b0d0295c237c766932b32
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-04-09 15:03:35 -0300

    Comando While

---
+++ python/Livro/comandoWhile.py
@@ -0,0 +1,19 @@
+#Em uma estrutura de repetição sempre haverá uma estrutura decisão, 
+#pois a repetição de um trecho de código sempre está associada a uma condição. 
+#Ou seja, um bloco de comandos será executado repetidas vezes, até que
+#uma condição não seja mais satisfeita.
+
+#O comando while deve ser utilizado para construir e controlar a 
+#estrutura decisão, sempre que o número de repetições não seja conhecido. 
+
+#Por exemplo, quando queremos solicitar e testar se o número digitado pelo usuário
+#é par ou ímpar. Quando ele digitar zero, o programa se encerra. 
+
+numero = 1
+
+while numero != 0:
+    numero = int(input("Digite um número: "))
+    if numero % 2 == 0:
+        print("Número par!\n")
+    else:
+        print("Número ímpar!\n")
