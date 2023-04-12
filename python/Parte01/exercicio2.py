839a3fd2ab5d5872cbc79437958988508d6ae1bf
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-04-12 12:02:22 -0300

    Atribuindo o valor retornado da função em uma variável

--- python/Parte01/exercicio2.py
+++ python/Parte01/exercicio2.py
@@ -3,3 +3,13 @@ def imprimir_mensagem(disciplina, curso):
     print(f"Minha primeira função em Python, desenvolvida na disciplina: {disciplina}, do curso: {curso}.")
 
 imprimir_mensagem("Linguagem de Programação", "Desenvolvimento Backend")
+
+#Para que o resultado de uma função possa ser guardado em uma variável, 
+#a função precisa ter o comando "return". A instrução "return", 
+#retorna um valor de uma função
+
+def imprimir_mensagem(disciplina, curso):
+    return f"Minha primeira função em Python, desenvolvida na disciplina: {disciplina}, do curso: {curso}."
+
+result=imprimir_mensagem("Linguagem de Programação", "Desenvolvimento Backend")
+print(result)
