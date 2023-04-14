23fc93c79662a350cdbcead4d2e08dbabad3096d
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-04-13 23:51:43 -0300

    Algumas operações de objetos do tipo sequência

---
+++ python/Livro/sequenciaCaracteres2.py
@@ -0,0 +1,5 @@
+texto = "Aprendendo Python na disciplina de linguagem de programação."
+
+print(f"\nTexto normal:",(texto))
+print(f"\nTexto com a função upper(): ",(texto.upper()))#que transforma para maiúsculo.
+print(f"Texto com a função replace(): ",(texto.replace("i", 'XX')))#usada para substiuir um caractere por outro
