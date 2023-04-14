36a5901a0c885f1f2c046d001bd527e2b7f8f415
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-04-13 23:57:54 -0300

    Função split()

---
+++ python/Livro/sequenciaCaracteres3.py
@@ -0,0 +1,7 @@
+texto = "Aprendendo Python na disciplina de linguagem de programação."
+print(f"texto = {texto}")
+print(f"Tamanho do texto = {len(texto)}\n")
+
+palavras = texto.split()
+print(f"palavras = {palavras}")
+print(f"Tamanho de palavras = {len(palavras)}")
