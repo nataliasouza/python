404831238236cff64a53a718f50b4a5840b80534
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-04-14 19:40:23 -0300

    List Comprehension - listcomp

--- python/Livro/listComprehension.py
+++ python/Livro/listComprehension.py
@@ -4,10 +4,14 @@
 #filtradas por um critério
 
 linguagens = ["Python", "Java", "JavaScript", "C", "C#", "C++", "Swift", "Go", "Kotlin"]
-#linguagens = '''Python Java JavaScript C C# C++ Swift Go Kotlin'''.split() # Essa sintaxe produz o mesmo resultado que a linha 1
+#A sintaxe comentada abaixo produz o mesmo resultado que a linha 6
+#linguagens = '''Python Java JavaScript C C# C++ Swift Go Kotlin'''.split() 
 
 print("Antes da listcomp = ", linguagens)
 
+#Dentro do colchetes há uma variável chamada "item" que representará cada valor da lista original
+#Veja que usamos item.lower() for item in linguagens, e o resultado foi guardado dentro da mesma 
+#variável original, ou seja, sobrescrevemos o valor de "linguagens"
 linguagens = [item.lower() for item in linguagens]
 
 print("\nDepois da listcomp = ", linguagens)
