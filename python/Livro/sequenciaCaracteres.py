5712b790db24454f6720959143ffa9306cba3eab
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-04-13 23:40:55 -0300

    Algumas operações de objetos do tipo sequência

--- python/Livro/sequenciaCaracteres.py
+++ python/Livro/sequenciaCaracteres.py
@@ -3,7 +3,8 @@
 texto = "Aprendendo Python na disciplina de linguagem de programação."
 
 print(f"{texto}")
-print(f"Tamanho do texto = {len(texto)}")
-print(f"Python in texto = {'Python' in texto}")
-print(f"Quantidade de y no texto = {texto.count('y')}")
-print(f"As 5 primeiras letras são: {texto[0:6]}")
+print(f"Tamanho do texto = {len(texto)}") #permite saber o tamanho da sequência
+print(f"Python in texto = {'Python' in texto}")#permite saber se um determinado valor está ou não na sequência
+print(f"Quantidade de y no texto = {texto.count('y')}")#permite contar a quantidade de ocorrências de um valor
+print(f"As 5 primeiras letras são: {texto[0:6]}")#notação com colchetes permite fatiar a sequência, exibindo somente partes dela. 
+
